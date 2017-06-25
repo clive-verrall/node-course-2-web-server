b7e7621f074efee4ddd5077c8358bb1713d669ff
Clive Verrall <clive_verrall@yahoo.co.uk>
2017-06-25 18:54:06 +0700

    Initial commit

---
+++ server.js
@@ -0,0 +1,61 @@
+/**
+ * Created by Clive on 15/05/2017.
+ */
+
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+
+
+app.use((req, res, next) => {
+    var now = new Date().toString();
+    var log = `${now}: ${req.method} ${req.url}`;
+    console.log(log);
+    fs.appendFile('server.log',log + '\n');
+    next();
+});
+
+app.use((req, res, next) => {
+   res.render('maintenance.hbs');
+});
+
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => {
+    return 'year: ' + new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+    return text.toUpperCase();
+});
+
+app.get('/', (req, res) => {
+    res.render('home.hbs', {
+        pageTitle: 'Home page',
+        welcomeMessage: 'Howdy, welcome to y\'all!'
+    });
+});
+
+app.get('/about', (req, res) => {
+   res.render('about.hbs', {
+       pageTitle: 'About page'
+   });
+});
+
+app.get('/bad', (req, res) => {
+    res.send( {
+        errorMessage: 'That was a bad request'
+        }
+
+    )
+});
+
+app.listen(3000, () => {
+    console.log("Server is up on port 3000");
+});
